536056160a2c287d36b7b7b9694f6c1e5d698425
Anthony Trombetta <atrombet@gmail.com>
2021-04-30 00:29:29 -0500

    Create custom select component and add placeholder styling.

--- src/components/NewMovieForm/NewMovieForm.tsx
+++ src/components/NewMovieForm/NewMovieForm.tsx
@@ -1,30 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Movie } from '../../interfaces';
 import './NewMovieForm.css';
 import { Rating } from '../Rating/Rating';
+import { ScreenItSelect } from '../ScreenItSelect/ScreenItSelect';
 import { CATEGORIES } from '../../constants';
+
 interface NewMovieFormProps {
   addMovie: (movie: Movie) => void
 }
 
 export const NewMovieForm: React.FC<NewMovieFormProps> = ({ addMovie }) => {
+  const [ newMovie, setNewMovie ] = useState<Movie>({ name: '', category: '', rating: 0 });
+
   return (
     <form className="newMovieForm">
       <div className="newMovieForm__grid">
-        <div className="newMovieForm__nameRow">
+        <div className="newMovieForm__gridRow newMovieForm__nameRow">
           <label>Name</label>
           <input placeholder="Name of the movie"/>
         </div>
-        <div className="newMovieForm__categoryRow">
+        <div className="newMovieForm__gridRow newMovieForm__categoryRow">
           <label>Category</label>
-          <select>
-            <option value="" disabled>Select a category</option>
-            {Object.keys(CATEGORIES).map(key => (
-              <option key={key} value={CATEGORIES[key].value}>{CATEGORIES[key].displayText}</option>
-            ))}
-          </select>
+          <ScreenItSelect options={Object.values(CATEGORIES)} selectedOption={''} onSelect={() => {}} />
         </div>
-        <div>
+        <div className="newMovieForm__gridRow">
           <label>Rating</label>
           <span className="newMovieForm__ratingCell">
             <Rating rating={0} editable={true} />
---
+++ src/components/ScreenItSelect/ScreenItSelect.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { SelectOption } from '../../interfaces';
+import './ScreenItSelect.css';
+
+interface ScreenItSelectProps {
+  options: SelectOption[],
+  selectedOption: string;
+  onSelect: (value: string) => void
+}
+
+export const ScreenItSelect: React.FC<ScreenItSelectProps> = ({ options, selectedOption, onSelect }) => {
+  // Define a function that sets the class name of the select to the class name of the currently selected option.
+  const setSelectClass = (select: any) => {
+    if (select) {
+      select.className = select.selectedOptions[0].className;
+    }
+  }
+
+  // Handle a ref callback when the select is created.
+  const selectRefCreated = (node: any) => {
+    setSelectClass(node);
+  }
+
+  // Handle a change event from the select.
+  const selectChange = (event: any) => {
+    const select = event.currentTarget;
+    setSelectClass(select);
+    onSelect(select.selectedOptions[0]);
+  }
+
+  return (
+    <div className="screenItSelect">
+      <select defaultValue="" ref={selectRefCreated} onChange={selectChange}>
+        <option className="placeholder" value="" disabled>Select a category</option>
+        {options.map(option => (
+          <option key={option.value} value={option.value}>{option.displayText}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
